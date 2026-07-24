37a5641f60ba109889ba948b8eaf58b23e548136
Azizur Rahaman <80843395+AzizurRahaman3870@users.noreply.github.com>
2026-07-24 09:03:36 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> frequencyHashS = new HashMap<>();
+        HashMap<Character, Integer> frequencyHashT = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            if(frequencyHashS.containsKey(s.charAt(i))) {
+                frequencyHashS.put(s.charAt(i), frequencyHashS.get(s.charAt(i)) + 1);
+            } else {
+                frequencyHashS.put(s.charAt(i), 1);
+            }
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            if(frequencyHashT.containsKey(t.charAt(i))) {
+                frequencyHashT.put(t.charAt(i), frequencyHashT.get(t.charAt(i)) + 1);
+            } else {
+                frequencyHashT.put(t.charAt(i), 1);
+            }
+        }
+
+        return frequencyHashS.equals(frequencyHashT);
+    }
+}
